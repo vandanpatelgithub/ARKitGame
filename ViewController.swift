631fe3a3bea9d0113ca904d78cc327aa2e339b08
Vandan Patel <vandan.ets@gmail.com>
2017-06-23 12:50:24 -0400

    First Commit

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ARKitGame
+//
+//  Created by Preeti Patel on 23/06/17.
+//  Copyright © 2017 Vandan Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
